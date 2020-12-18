aefd6bdbe8a09ac22799718545a175230c49ec31
Home <allosipyan@mail.ru>
2020-12-18 20:39:04 +0500

    redis

--- Consumer/src/main/java/com/example/consumerPackage/Consumer.java
+++ Consumer/src/main/java/com/example/consumerPackage/Consumer.java
@@ -2,20 +2,37 @@ package com.example.consumerPackage;
 
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.Arrays;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class Consumer {
+        private ValueOperations<String, Integer> valueOperations;
+
+        private RedisTemplate<String, Integer> redisTemplate;
+
+        public Consumer(RedisTemplate<String, Integer> redisTemplate){
+            this.redisTemplate = redisTemplate;
+            this.valueOperations = this.redisTemplate.opsForValue();
+        }
+
+        public void save(String url, int code){
+            valueOperations.set(url, code, 120, TimeUnit.SECONDS);;
+        }
+
+        public int findByKey(String url){
+            Object value = valueOperations.get(url);
+            return value != null ?  (int)value: -1;
+
+        }
 
         @RabbitListener(queues = "link1")
         public void receive1(String in) throws InterruptedException {
@@ -23,36 +40,49 @@ public class Consumer {
         }
 
 
-        public void receive(String in, int receiver) {
-            System.out.println("instance " + receiver + " [x] Received '" + in + "'");
-            Long inLinkId = Long.parseLong(in.substring(in.indexOf("linkId")+8, in.indexOf("url")-2));
-            String inUrl = in.substring(in.indexOf("url")+6, in.length()-2).replace("\\", "");
-            System.out.println(inLinkId+ "aaa"+ inUrl);
+    public void receive(String in, int receiver) {
+        System.out.println("instance " + receiver + " [x] Received '" + in + "'");
+        Long inLinkId = Long.parseLong(in.substring(in.indexOf("linkId")+8, in.indexOf("url")-2));
+        String inUrl = in.substring(in.indexOf("url")+6, in.length()-2).replace("\\", "");
+        System.out.println(inLinkId+ "aaa"+ inUrl);
+        int status = findByKey(inUrl);
+        System.out.println(this.redisTemplate);
+        System.out.println(this.valueOperations);
+        if(status == -1) {
             try {
                 URL url = new URL(inUrl);
                 HttpURLConnection con = (HttpURLConnection) url.openConnection();
                 con.setRequestMethod("GET");
                 int code = con.getResponseCode();
                 System.out.println(code);
-                try {
-                    URL appUrl = new URL("http://192.168.99.100:8080/status/link");
-                    HttpURLConnection appConnection = (HttpURLConnection) appUrl.openConnection();
-                    appConnection.setRequestMethod("PUT");
-                    appConnection.setDoOutput(true);
-                    appConnection.setRequestProperty("Content-Type", "application/json");
-                    appConnection.setRequestProperty("Accept", "application/json");
-                    OutputStreamWriter osw = new OutputStreamWriter(appConnection.getOutputStream());
-                    String str = "{\"id\":"+inLinkId+",\"status\":"+code+",\"url\":\""+inUrl+"\"}";
-                    System.out.println(str);
-                    osw.write(str);
-                    osw.flush();
-                    osw.close();
-                    System.out.println("appCode "+appConnection.getResponseCode());
-                } catch (IOException e) {
-                    System.out.println(Arrays.toString(e.getStackTrace()));
-                }
+                sendMessage(inLinkId, code, inUrl);
+                save(inUrl, code);
             } catch (Exception e) {
                 System.out.println(Arrays.toString(e.getStackTrace()));
             }
         }
+        else {
+            sendMessage(inLinkId, status, inUrl);
+        }
+
+    }
+    private void sendMessage(Long inLinkId, int code, String inUrl) {
+        try {
+            URL appUrl = new URL("http://192.168.99.100:8080/status/link");
+            HttpURLConnection appConnection = (HttpURLConnection) appUrl.openConnection();
+            appConnection.setRequestMethod("PUT");
+            appConnection.setDoOutput(true);
+            appConnection.setRequestProperty("Content-Type", "application/json");
+            appConnection.setRequestProperty("Accept", "application/json");
+            OutputStreamWriter osw = new OutputStreamWriter(appConnection.getOutputStream());
+            String str = "{\"id\":" + inLinkId + ",\"status\":" + code + ",\"url\":\"" + inUrl + "\"}";
+            System.out.println(str);
+            osw.write(str);
+            osw.flush();
+            osw.close();
+            System.out.println("appCode " + appConnection.getResponseCode());
+        } catch (IOException e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+    }
 }
--- Consumer/src/main/java/com/example/consumerPackage/ConsumerApplication.java
+++ Consumer/src/main/java/com/example/consumerPackage/ConsumerApplication.java
@@ -8,16 +8,22 @@ import org.springframework.amqp.core.FanoutExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import java.io.IOException;
 @Configuration
+@EnableCaching
 @SpringBootApplication
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
     }
+
     @Bean
     public FanoutExchange fanout() {
         return new FanoutExchange("links");
@@ -36,6 +42,20 @@ public class ConsumerApplication {
 
     @Bean
     public Consumer receiver() {
-        return new Consumer();
+        return new Consumer(redisTemplate());
+    }
+
+    @Bean
+    public RedisTemplate<String, Integer> redisTemplate() {
+        RedisTemplate<String, Integer> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(System.getenv("REDIS_CONNECTION"));
+        System.out.println(System.getenv("REDIS_CONNECTION"));
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 }
